e3d693aac1fc842bd8cfb1cd5cd74d63a027ef59
Anurag Sinha <anurag3487@gmail.com>
2021-08-26 14:14:16 +0530

    Created EditCourse form, GetCourseById API called and values bind

--- app/src/components/APIs/API.js
+++ app/src/components/APIs/API.js
@@ -1,5 +1,6 @@
 const API = {
   ALL_COURSES_API: `https://localhost:5001/GetAllCourses`,
+  GET_COURSE_BY_ID: `https://localhost:5001/GetCoursesById/`,
 };
 
 export default API;
--- app/src/components/EditCourse/EditCourse.js
+++ app/src/components/EditCourse/EditCourse.js
@@ -1,17 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import API from "../APIs/API";
+import "./styles.css";
 
 const EditCourse = () => {
   const { id } = useParams();
   const history = useHistory();
+  const emptyCourse = {
+    id: "",
+    courseName: "",
+    courseContent: "",
+    courseBook: "",
+    noOfStudents: "",
+    coordinatorName: "",
+    createdOn: "",
+  };
+  const [courseData, setCourseData] = useState(emptyCourse);
+
+  useEffect(() => {
+    let apiUrl = `${API.GET_COURSE_BY_ID}${id}`;
+
+    const fetchData = async () => {
+      let response = await fetch(apiUrl);
+      let data = await response.json();
+      setCourseData(data);
+    };
+    fetchData();
+  }, [id]);
+
   console.log(history);
   return (
-    <div>
+    <div className="edit-courses">
       <h1>Edit Course</h1>
-      <p>the passed id parameter is :- {id}</p>
-      <button className="btn btn-outline-info" onClick={history.goBack}>
-        Back
-      </button>
+      <form className="row g-3">
+        <div className="col-md-3">
+          <label className="form-label">Id</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.id}
+            readOnly
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Course Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.courseName}
+            readOnly
+          />
+        </div>
+        <div className="col-9">
+          <label className="form-label">Course Content</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.courseContent}
+            readOnly
+          />
+        </div>
+        <div className="col-12">
+          <label className="form-label">Course Book Img Url</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.courseBook}
+            readOnly
+          />
+        </div>
+        <div className="col-md-3">
+          <label className="form-label">Number of students enrolled</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.noOfStudents}
+            readOnly
+          />
+        </div>
+        <div className="col-md-3">
+          <label className="form-label">Coordinator Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.coordinatorName}
+            readOnly
+          />
+        </div>
+        <div className="col-md-3">
+          <label className="form-label">Start Date</label>
+          <input
+            type="text"
+            className="form-control"
+            value={courseData.createdOn}
+            readOnly
+          />
+        </div>
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
+        </div>
+        <div className="col-12">
+          <button className="btn btn-dark" onClick={history.goBack}>
+            Back
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
